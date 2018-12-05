1a3ee0aa18251b940ff0da825731a760c69cc05d
Travis <travis.barton@sjsu.edu>
2018-12-04 18:04:53 -0800

    update

--- Physics Network (trial).py
+++ Physics Network (trial).py
@@ -11,12 +11,18 @@ from sklearn import svm
 import keras
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.layers import Dropout
+from keras.callbacks import ModelCheckpoint
 import math
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
 import spacy
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.metrics import confusion_matrix
+from random import choice, sample
+
+
 
 def Turn_into_Spacy(data, tags = False):
     docs = []
@@ -66,11 +72,31 @@ def Percent(truth, test):
     te = Pred_to_num(test)
     for i in range(truth.shape[0]):
         if tr[i] == te[i]:
-            print("{} = {}".format(tr[i], te[i]))
             correct += 1
     return(correct/truth.shape[0])
 
     
+
+def Noise_maker(data, sub):
+    isindex = np.where(noise.iloc[:,2] == 'physics')
+    isntindex = np.where(data.iloc[:,2] != sub)
+    isindex = sample(list(isindex[0]), 1000)
+    isntindex = sample(list(isntindex[0]), 2000)
+    index = [isindex, isntindex]
+    final = pd.DataFrame(columns = ['id', 'Title', 'tag'])
+    temp = [data.iloc[i,:] for i in isindex]
+    temp2 = [data.iloc[i,:] for i in isntindex]
+    k = 0
+    for i in temp:
+        final.loc[k,:] = i.values
+        k = k+1
+    for i in temp2:
+        final.loc[k,:] = i.values
+        k = k+1
+    return(final)
+    
+  
+
 def plot_confusion_matrix(cm, classes,
                           normalize=False,
                           title='Confusion matrix',
@@ -106,18 +132,27 @@ def plot_confusion_matrix(cm, classes,
     plt.tight_layout()
 
 data = pd.read_csv("/Users/travisbarton/Redditbot/askscience_Data.csv")
+noise = pd.read_csv("/Users/travisbarton/Redditbot/Training_data.csv")
 data = data.iloc[:, 1:]
+noise = noise.iloc[:,1:]
+noise = Noise_maker(noise, 'physics')
 nlp = spacy.load('en_vectors_web_lg')
 
 Layer2_Spacy_vector = Turn_into_Spacy(data)
+Layer1_Spacy_vector = Turn_into_Spacy(noise)
 data.tag = Sub_treater(data.tag, 'physics')
+noise.iloc[:,2] = Sub_treater(noise.iloc[:,2], 'physics')
 
-dat = np.empty([data.shape[0], 301])
-for i in range(data.shape[0]):
+
+dat = np.empty([(data.shape[0]+noise.shape[0]), 301])
+for i in range(data.shape[0]+noise.shape[0]):
     for j in range(300):
-        dat[i,j] = Layer2_Spacy_vector[i][j]
-        
-dat[:, 300] = pd.factorize(data.tag)[0]
+        if i < data.shape[0]:
+            dat[i,j] = Layer2_Spacy_vector[i][j]
+        else:
+            dat[i,j] = Layer1_Spacy_vector[i - data.shape[0]][j]
+dat[0:data.shape[0], 300] = pd.factorize(data.tag)[0]
+dat[data.shape[0]:,300] = pd.factorize(noise.iloc[:,2])[0]
 onehot_encoder = OneHotEncoder(sparse=False)       
         
 X_train, X_test, y_train, y_test = train_test_split(dat[:,:300], dat[:,300], test_size=0.33, random_state=42)   
@@ -155,12 +190,15 @@ plt.subplot(212)
 plt.plot(model_history.history['loss'])
 plt.plot(model_history.history['val_loss'])
 
-preds = model.predict(X_test[:,:300])
+model.load_weights("Physics_Models/weights-improvement-0.92.hdf5")
+
+physpreds = model.predict(X_test[:,:300])
 
 
-Percent(y_test, preds)        
-confm = confusion_matrix(Pred_to_num(y_test), Pred_to_num(preds))
-plot_confusion_matrix(confm, [0,1])
+Percent(y_test, physpreds)        
+confm = confusion_matrix(Pred_to_num(y_test), Pred_to_num(physpreds))
+confm/sum(sum(confm))
+plot_confusion_matrix(confm, [0,1], normalize = True)
 
 
 
